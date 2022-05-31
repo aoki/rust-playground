e1c2c5ac52794d6891fbc6b9034045282e217bbc
aoki <aoki@aoki.dev>
2022-05-31 23:15:14 +0900

    add packet cap

---
+++ packet-capture/src/main.rs
@@ -0,0 +1,16 @@
+use std::env;
+use log::error;
+use pnet::datalink;
+
+fn main() {
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
+    let args: Vec<String> =env::args().collect();
+    if args.len() != 2 {
+        error!("Please specify target interface name");
+        std::process::exit(1);
+    }
+    let interface_name = &args[1];
+    let interfaces = datalink::interfaces();
+}
