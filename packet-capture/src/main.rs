fd84acc12c5667b17931eb2e57e6eefbc470825e
aoki <aoki@aoki.dev>
2022-06-01 23:23:20 +0900

    update

--- packet-capture/src/main.rs
+++ packet-capture/src/main.rs
@@ -1,16 +1,20 @@
-use std::env;
 use log::error;
 use pnet::datalink;
+use std::env;
 
 fn main() {
     env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
-    let args: Vec<String> =env::args().collect();
+    let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         error!("Please specify target interface name");
         std::process::exit(1);
     }
     let interface_name = &args[1];
     let interfaces = datalink::interfaces();
+    let interface = interfaces
+        .into_iter()
+        .find(|iface| iface.name == *interface_name)
+        .expect("Failed to get interface");
 }
