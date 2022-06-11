f17dd9b37e610456ab8f1bf0770e1a6bba09b928
aoki <aoki@aoki.dev>
2022-06-11 21:26:56 +0900

    add packet info

--- port-scanner/src/main.rs
+++ port-scanner/src/main.rs
@@ -1,6 +1,6 @@
 use log::error;
 use pnet::packet::tcp::TcpFlags;
-use std::{env, net::Ipv4Addr, process};
+use std::{collections::HashMap, env, fs, net::Ipv4Addr, process};
 
 enum ScagnType {
     Syn = TcpFlags::SYN as isize,
@@ -25,5 +25,18 @@ fn main() {
         error!("Bad number of arguments. [ipaddr] [scantype]");
         process::exit(1);
     }
+
+    let packet_info = {
+        let contents = fs::read_to_string(".env").expect("Failed to read env file");
+        let lines: Vec<_> = contents.split('\n').collect();
+        let mut map = HashMap::new();
+        for line in lines {
+            let elm: Vec<_> = line.split('=').map(str::trim).collect();
+            if elm.len() == 2 {
+                map.insert(elm[0], elm[1]);
+            }
+        }
+    };
+
     println!("Hello, world!");
 }
